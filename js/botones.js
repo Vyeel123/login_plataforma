6517c331ad2b4aa934615f7b4919a3d92b0dbbbe
Sebastian <90334815+Vyeel123@users.noreply.github.com>
2022-10-21 15:00:57 -0400

    Add files via upload

---
+++ js/botones.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("loginEstudiante");
+var y = document.getElementById("loginDocente");
+var z = document.getElementById("elegir")
+
+function loginEstudiante(){
+    x.style.left = "50px";
+    y.style.left= "450px";
+    z.style.left ="0";
+}
+
+function loginDocente(){
+    x.style.left = "-450px";
+    y.style.left ="50px"
+    z.style.left ="120px"
+}
